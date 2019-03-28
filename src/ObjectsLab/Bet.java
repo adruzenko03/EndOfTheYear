05802ca070c12360e97feaafc4af3f1b93ae6b3f
adruz <adruzenko03@gmail.com>
2019-03-28 09:38:13 -0400

    font

---
+++ src/ObjectsLab/Bet.java
@@ -0,0 +1,15 @@
+package ObjectsLab;
+
+public class Bet {
+    private double money;
+    private Team pickedTeam;
+    private Team otherTeam;
+    private Boolean betWon;
+    public Bet(double money, Team pickedTeam,Team otherTeam){
+        this.money=money;
+        this.otherTeam=otherTeam;
+        this.pickedTeam=pickedTeam;
+    }
+
+
+}
