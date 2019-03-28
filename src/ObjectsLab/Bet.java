bb22fd9d8f6864c5fef4cb73ad188ee280e788fa
vigne <vignet009@gmail.com>
2019-03-28 09:52:45 -0400

    i tried my best

--- src/ObjectsLab/Bet.java
+++ src/ObjectsLab/Bet.java
@@ -5,6 +5,7 @@ public class Bet {
     private Team pickedTeam;
     private Team otherTeam;
     private Boolean betWon;
+
     public Bet(double money, Team pickedTeam,Team otherTeam){
         this.money=money;
         this.otherTeam=otherTeam;
